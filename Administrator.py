c2f3f0172718f07e696b897c71f3da7af2b2fd4c
niko13072 <nicolasosorioarias5@gmail.com>
2022-05-24 21:40:25 -0500

    class

--- Administrator.py
+++ Administrator.py
@@ -1,5 +1,9 @@
+import email
 from User import User
 
-class Administrador():
-    self.User = user 
-    self.Name = name
+class Administrador(User):
+    def __init__(self, user:User, name:str, email:str):
+        self.User = user 
+        self.Name = name
+        self.Email = email
+    
